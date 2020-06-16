9994cf6ee75fffeaae277ce6f0ca8979a6691560
Tom Sidebottom <tom@sidebottom.net>
2020-06-16 12:28:15 -0500

    Add row selection.

--- Crachet/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ Crachet/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  selectedForecast?: WeatherForecast;
 
   constructor(private http: HttpClient,
     @Inject('BASE_URL') private baseUrl: string) {
@@ -18,12 +19,29 @@ export class FetchDataComponent {
     this.http.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast').subscribe(
       result => {
         this.forecasts = result;
+        this.forecasts.forEach(forecast => {
+          const forecastDate = new Date(forecast.date);
+          forecast.dateFormattedString = forecastDate.toLocaleString();
+
+        });
+
+       /* for (const forecast of result) {
+          const forecastDate = new Date(forecast.date);
+          forecast.dateFormattedString = forecastDate.toLocaleString();
+        }*/
+
     }, error => console.error(error));
   }
+
+  selectRow(selectedRowNumber: number) {
+    this.selectedForecast = this.forecasts[selectedRowNumber];
+  }
+
 }
 
 interface WeatherForecast {
-  date: string;
+  date: string; // ISO date string, not user-friendly
+  dateFormattedString: string;
   temperatureC: number;
   temperatureF: number;
   summary: string;
